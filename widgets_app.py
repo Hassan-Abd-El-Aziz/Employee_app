1a91cf6e66ee2aebb7cf70328382a5ac42ae4814
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-11-23 14:58:29 +0200

    add lable and entry section

--- widgets_app.py
+++ widgets_app.py
@@ -22,6 +22,9 @@ class WidgetsApp:
 # تشغيل الدالة الخاصة بإضافة مكان البحث
         self.call_function_add_search_frame=Thread(target=self.add_search_frame,args=(),)
         self.call_function_add_search_frame.start()
+# تشغيل الدالة الخاصة بحقل إدخال البحث
+        self.call_function_add_entry_search=Thread(target=self.add_entry_search,args=(),)
+        self.call_function_add_entry_search.start()
 
 # الداخلة الخاصة بالعنوان
     def add_frame_title(self):
@@ -37,3 +40,13 @@ class WidgetsApp:
         '''add search frame employee'''
         self.search_frame=Frame(self.master,width=1165,height=80,background="#d9ddf7",relief=SUNKEN,border=2)
         self.search_frame.place(x=18,y=88)
+        self.search_label=Label(self.master,
+                                background="#d9ddf7",text="من فضلك أدخل الرقم الوظيفي للموظف أو البريد الإلكتروني للبحث عنه",font=('Bold Oblique',18))
+        self.search_label.place(x=600,y=113)
+    def add_entry_search(self):
+        '''add entry search for employee'''
+        self.search_employee=Entry(self.master
+                                   ,width=33,background="#E6F2FC",
+                                   font=('Bold Oblique',18)
+                                   ,relief=RIDGE,border=1,justify=CENTER)
+        self.search_employee.place(x=160,y=113)
