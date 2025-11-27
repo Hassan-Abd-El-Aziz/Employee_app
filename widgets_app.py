598bcfdf82d1dc819c6bfdd4f736cb7e1cfe734b
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-11-27 15:53:38 +0200

    add buttons section

--- widgets_app.py
+++ widgets_app.py
@@ -40,7 +40,23 @@ class WidgetsApp:
         self.add_city()
         self.add_name()
         self.add_date()
+
+        self.add_Image_employee()
+
+        self.add_salary()
+        self.add_age()
 # ====================================================== #
+# ====================================================== #
+        self.add_frame_buttons()
+        self.func_insert()
+        self.func_update()
+        self.func_delete()
+        self.func_show()
+        self.func_empty()
+# ====================================================== #
+# ====================================================== #
+
+
 
 # الدالة الخاصة بالعنوان
     def add_frame_title(self):
@@ -149,7 +165,7 @@ class WidgetsApp:
         self.label_section.place(x=420,y=291)
 
         self.entry_section=Entry(self.master,width=20,font=('Bold Oblique',18),
-                                 relief=RAISED,border=1,background="#EDFFF0")
+                                 relief=RAISED,border=1,background="#EDFFF0",justify=RIGHT)
         self.entry_section.place(x=120,y=292)
     
 
@@ -173,7 +189,7 @@ class WidgetsApp:
         self.entry_city=Entry(self.master,width=20,
                               font=('Bold Oblique',18),
                                  relief=RAISED,border=1,
-                                 background="#EDFFF0")
+                                 background="#EDFFF0",justify=RIGHT)
         self.entry_city.place(x=120,y=360)
     
     def add_name(self):
@@ -185,9 +201,10 @@ class WidgetsApp:
         self.entry_name=Entry(self.master,width=22,
                               font=('Bold Oblique',18),
                                  relief=RAISED,border=1,
-                                 background="#EDFFF0")
+                                 background="#EDFFF0",justify=RIGHT)
         self.entry_name.place(x=520,y=433)
 
+# إضافة التاريخ
     def add_date(self):
         '''add date'''
         self.label_date=Label(self.master,
@@ -202,7 +219,7 @@ class WidgetsApp:
                                   month=datetime.now().month,
                                   day=datetime.now().day,
                                   font=('Bold Oblique',18),
-                                  justify=CENTER,
+                                  justify='center',
                                   date_pattern="yyyy-mm-dd",
                                   foreground="black",
                                   headersforground="black",
@@ -210,3 +227,118 @@ class WidgetsApp:
                                   width=18,
                                   )
         self.entry_date.place(x=110,y=433)
+# إضافة الصورة والزرار 
+    def add_Image_employee(self):
+        '''add image for employee'''
+        self.image=Image.open("images\\image_empoyee.jpg")
+        self.image=self.image.resize((170,160))
+
+        self.insert_image_employee=ImageTk.PhotoImage(self.image)
+        self.label_insert_image=Label(self.master,
+                                      image=self.insert_image_employee).place(x=995,y=300)
+     
+     
+        self.image_open=Image.open("images\\open_file.png")
+        self.image_open=self.image_open.resize((30,30))
+
+        self.insert_image_open=ImageTk.PhotoImage(self.image_open)
+        self.button_choice=Button(self.master,text="إختيار صورة",relief=GROOVE
+                                  ,width=130,height=30,border=2,
+                                  background="#EDFFF0"
+                                  ,font=('Bold Oblique',18), compound='left',
+                                  padx=20,image=self.insert_image_open)
+        self.button_choice.place(x=995,y=470)
+        
+# اضافة الراتب
+    def add_salary(self):
+        self.label_salary=Label(self.master,text="الراتب",
+                                font=('Bold Oblique',18),
+                                background="#AEDEFC").place(x=890,y=510)
+        self.entry_salary=Entry(self.master,width=22,
+                            font=('Bold Oblique',18),
+                            relief=RAISED,border=1,
+                            background="#EDFFF0",justify=CENTER)
+        self.entry_salary.place(x=520,y=510)
+    
+# إضافة العمر
+    def add_age(self):
+        self.label_age=Label(self.master,text="السن",font=('Bold Oblique',18),
+                             background="#AEDEFC",).place(x=420,y=510)
+        self.entry_age=Entry(self.master,width=16,
+                            font=('Bold Oblique',18),
+                            relief=RAISED,border=1,
+                            background="#EDFFF0",justify=CENTER).place(x=150,y=510)
+# اضافة قسم الزراير 
+    def add_frame_buttons(self):
+        self.frame_buttons=Frame(self.master,width=1165,height=106,background="#AEDEFC",
+                                 padx=10,pady=3
+                                 ,relief=RIDGE,border=2).place(x=18,y=600)
+        # زرار اضافة البيانات
+    def func_insert(self):
+        self.image_insert=Image.open("images\\insert_data.png")
+        self.image_insert= self.image_insert.resize((30,30))
+
+        self.show_image_insert=ImageTk.PhotoImage(self.image_insert)
+        self.button_insert=Button(self.master,
+                                  text="إضافة الموظف",width=145,height=40,
+                                  relief=FLAT,border=2,
+                                  image=self.show_image_insert,
+                                  font=('Bold Oblique',18),padx=20,compound="left")
+        self.button_insert.place(x=976,y=630)
+
+                                  
+        # زرار تحديث البيانات
+    def func_update(self):
+        self.image_update=Image.open("images\\update_data.png")
+        self.image_update= self.image_update.resize((30,30))
+
+        self.show_image_update=ImageTk.PhotoImage(self.image_update)
+        self.button_update=Button(self.master,
+                                  text="تحديث البيانات",width=145,height=40,
+                                  relief=FLAT,border=2,
+                                  image=self.show_image_update,
+                                  font=('Bold Oblique',18),padx=20,compound="left")
+        self.button_update.place(x=740,y=630)
+
+
+          # زرار حذف البيانات
+    def func_delete(self):
+        self.image_delete=Image.open("images\\delete_data.png")
+        self.image_delete= self.image_delete.resize((30,30))
+
+        self.show_image_delete=ImageTk.PhotoImage(self.image_delete)
+        self.button_delete=Button(self.master,
+                                  text="حذف البيانات",width=145,height=40,
+                                  relief=FLAT,border=2,
+                                  image=self.show_image_delete,
+                                  font=('Bold Oblique',18),padx=20,compound="left")
+        self.button_delete.place(x=500,y=630)
+    
+
+         # زرار افراغ الحقول   
+    def func_empty(self):
+        self.image_empty=Image.open("images\\empty_data.png")
+        self.image_empty= self.image_empty.resize((30,30))
+
+        self.show_image_empty=ImageTk.PhotoImage(self.image_empty)
+        self.button_empty=Button(self.master,
+                                  text="إفراغ الحقول",width=145,height=40,
+                                  relief=FLAT,border=2,
+                                  image=self.show_image_empty,
+                                 font=('Bold Oblique',18),padx=20,compound="left")
+        self.button_empty.place(x=260,y=630)
+
+
+
+         # زرار عرض البيانات  
+    def func_show(self):
+        self.image_show=Image.open("images\\show_data.png")
+        self.image_show= self.image_show.resize((30,30))
+
+        self.show_image_show=ImageTk.PhotoImage(self.image_show)
+        self.button_show=Button(self.master,
+                                  text="عرض البيانات",width=145,height=40,
+                                  relief=FLAT,border=2,
+                                  image=self.show_image_show,
+                                  font=('Bold Oblique',18),padx=20,compound="left")
+        self.button_show.place(x=35,y=630)
